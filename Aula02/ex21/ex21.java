71887857ad87dd50b084881597f3bcae33445b0a
BuildTools <unconfigured@null.spigotmc.org>
2016-09-28 22:51:02 +0100

    Aula 2,beta

--- Aula02/ex21/ex21.java
+++ Aula02/ex21/ex21.java
@@ -7,31 +7,42 @@ public class ex21{
 	private static Scanner sc = new Scanner(System.in);
 
 	private static int serveMenu(){
-		System.out.printf("1-Add Student\n2-Add Staff\n");
+		System.out.printf("\n1-Add Student\n2-Add Staff\n");
 		System.out.printf("3-Add Movie\n");
 		System.out.printf("4-Remove Client\n5-Remove Movie\n");
 		System.out.printf("6-List Students\n7-List Staff\n");
 		System.out.printf("8-List Movies (ordered by rating)\n9-List Client history\n");
 		System.out.printf("10-Checkout Movie\n11-Checkin Movie\n");
 		System.out.printf("0-Exit\n");
-		return Integer.parseInt(sc.nextLine());
+		try{
+			return Integer.parseInt(sc.nextLine());
+		}
+		catch(Exception e){
+			return -1;
+		}
 	}
 	private static void addStudent() throws ParseException{
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 		String name;
 		Date dnasc;
 		int cc,nmec,course;
-		System.out.printf("\nName:");		
-		name=sc.nextLine();
-		System.out.printf("\nDate of birth (DD/MM/YYYY):");		
-		dnasc=df.parse(sc.nextLine());
-		System.out.printf("\nNational identification number:");		
-		cc=Integer.parseInt(sc.nextLine());
-		System.out.printf("\nMechanography number:");		
-		nmec=Integer.parseInt(sc.nextLine());
-		System.out.printf("\nCourse number:");		
-		course=Integer.parseInt(sc.nextLine());
-		mov_man.addStudent(new Student(name,new Date(),dnasc,cc,nmec,course));
+		try{
+			System.out.printf("\nName:");		
+			name=sc.nextLine();
+			System.out.printf("\nDate of birth (DD/MM/YYYY):");		
+			dnasc=df.parse(sc.nextLine());
+			System.out.printf("\nNational identification number:");		
+			cc=Integer.parseInt(sc.nextLine());
+			System.out.printf("\nMechanography number:");		
+			nmec=Integer.parseInt(sc.nextLine());
+			System.out.printf("\nCourse number:");		
+			course=Integer.parseInt(sc.nextLine());
+			mov_man.addStudent(new Student(name,new Date(),dnasc,cc,nmec,course));
+		}
+		catch(Exception e){
+			System.out.printf("\nInput mismatch.\n");
+			return;
+		}
 		return;
 	}
 	private static void addStaff() throws ParseException{
@@ -39,37 +50,55 @@ public class ex21{
 		String name;
 		Date dnasc;
 		int cc,nfunc,nif;
-		System.out.printf("\nName:");		
-		name=sc.nextLine();
-		System.out.printf("\nDate of birth (DD/MM/YYYY):");		
-		dnasc=df.parse(sc.nextLine());
-		System.out.printf("\nNational identification number:");		
-		cc=Integer.parseInt(sc.nextLine());
-		System.out.printf("\nStaff number:");		
-		nfunc=Integer.parseInt(sc.nextLine());
-		System.out.printf("\nFiscal id:");		
-		nif=Integer.parseInt(sc.nextLine());
-		mov_man.addStaff(new Staff(name,new Date(),dnasc,cc,nfunc,nif));
+		try{
+			System.out.printf("\nName:");		
+			name=sc.nextLine();
+			System.out.printf("\nDate of birth (DD/MM/YYYY):");		
+			dnasc=df.parse(sc.nextLine());
+			System.out.printf("\nNational identification number:");		
+			cc=Integer.parseInt(sc.nextLine());
+			System.out.printf("\nStaff number:");		
+			nfunc=Integer.parseInt(sc.nextLine());
+			System.out.printf("\nFiscal id:");		
+			nif=Integer.parseInt(sc.nextLine());
+			mov_man.addStaff(new Staff(name,new Date(),dnasc,cc,nfunc,nif));
+		}
+		catch(Exception e){
+			System.out.printf("\nInput mismatch.\n");
+			return;
+		}
 		return;
 	}
 	private static void addMovie(){
 		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 		String name,rate,genre;
-		System.out.printf("\nName:");		
-		name=sc.nextLine();
-		System.out.printf("\nAge Rating:"); 
-		rate=sc.nextLine();
-		System.out.printf("\nGenre:");		
-		genre=sc.nextLine();
-		mov_man.addMovie(new Movie(name,rate,genre));
+		try{
+			System.out.printf("\nName:");		
+			name=sc.nextLine();
+			System.out.printf("\nAge Rating:"); 
+			rate=sc.nextLine();
+			System.out.printf("\nGenre:");		
+			genre=sc.nextLine();
+			mov_man.addMovie(new Movie(name,rate,genre));
+		}
+		catch(Exception e){
+			System.out.printf("\nInput mismatch.\n");
+			return;
+		}
 		return;
 	}
 	private static void removeClient(){
 		int id;
 		String name;
-		System.out.printf("\nUnique ID:");		
-		id=Integer.parseInt(sc.nextLine());
-		name=mov_man.removeClient(id);
+		try{
+			System.out.printf("\nUnique ID:");		
+			id=Integer.parseInt(sc.nextLine());
+			name=mov_man.removeClient(id);
+		}
+		catch(Exception e){
+			System.out.printf("\nInput mismatch.\n");
+			return;
+		}
 		if(name==null)
 			System.out.printf("No one was found with that ID.\n");
 		else
@@ -79,9 +108,15 @@ public class ex21{
 	private static void removeMovie(){
 		int id;
 		String name;
-		System.out.printf("\nUnique ID:");		
-		id=Integer.parseInt(sc.nextLine());
-		name=mov_man.removeMovie(id);
+		try{
+			System.out.printf("\nUnique ID:");		
+			id=Integer.parseInt(sc.nextLine());
+			name=mov_man.removeMovie(id);
+		}
+		catch(Exception e){
+			System.out.printf("\nInput mismatch.\n");
+			return;
+		}
 		if(name==null)
 			System.out.printf("No one was found with that ID.\n");
 		else
@@ -90,10 +125,16 @@ public class ex21{
 	}
 	private static void checkInMovie(){
 		int movId,rating;
-		System.out.printf("\nUnique movie ID:");		
-		movId=Integer.parseInt(sc.nextLine());
-		System.out.printf("\nMovie rating (1-10):");		
-		rating=Integer.parseInt(sc.nextLine());
+		try{
+			System.out.printf("\nUnique movie ID:");		
+			movId=Integer.parseInt(sc.nextLine());
+			System.out.printf("\nMovie rating (1-10):");		
+			rating=Integer.parseInt(sc.nextLine());
+		}
+		catch(Exception e){
+			System.out.printf("\nInput mismatch.\n");
+			return;
+		}
 		if(mov_man.checkInMovie(movId,rating))
 			System.out.printf("Sucesss.\n");
 		else
@@ -103,11 +144,17 @@ public class ex21{
 	}
 	private static void checkOutMovie(){
 		int clId,movId,result;
-		System.out.printf("\nUnique client ID:");		
-		clId=Integer.parseInt(sc.nextLine());
-		System.out.printf("\nUnique movie ID:");		
-		movId=Integer.parseInt(sc.nextLine());
-		result=mov_man.checkOutMovie(movId,clId);
+		try{
+			System.out.printf("\nUnique client ID:");		
+			clId=Integer.parseInt(sc.nextLine());
+			System.out.printf("\nUnique movie ID:");		
+			movId=Integer.parseInt(sc.nextLine());
+			result=mov_man.checkOutMovie(movId,clId);
+		}
+		catch(Exception e){
+			System.out.printf("\nInput mismatch.\n");
+			return;
+		}
 		if(result==0)
 			System.out.printf("Sucess.\n");
 		else if(result==-1)
@@ -121,9 +168,15 @@ public class ex21{
 	private static void clientHist(){
 		int clId;
 		String str;
-		System.out.printf("\nUnique client ID:");
-		clId=Integer.parseInt(sc.nextLine());
-		str=mov_man.clientRequestHistToString(clId);
+		try{
+			System.out.printf("\nUnique client ID:");
+			clId=Integer.parseInt(sc.nextLine());
+			str=mov_man.clientRequestHistToString(clId);
+		}
+		catch(Exception e){
+			System.out.printf("\nInput mismatch.\n");
+			return;
+		}
 		if(str!=null){
 			System.out.printf("\n"+str+"\n");
 			return;
