993b03f83ebfb098b74486aae5f6426bff1d76c7
Manuel Neves <msneves_97@hotmail.com>
2016-10-20 22:38:42 +0100

    Exercício 1 da Aula 5 - editado

--- Aula05/ex51/UtilCompare.java
+++ Aula05/ex51/UtilCompare.java
@@ -1,21 +1,21 @@
-public abstract class UtilCompare{
+public abstract class UtilCompare {
 	
-	public static Comparable findMax(Comparable[] t){
-		Comparable max = t[0];
-		for(Comparable c:t)
+	public static <T extends Comparable<? super T>> T findMax(T[] t){
+		T max = t[0];
+		for(T c:t)
 			if(c.compareTo(max)>0)
 				max=c;
 		return max;
 	}
 	
-	public static void sortArray(Comparable[] t){
+	public static <T extends Comparable<? super T>> void sortArray(T[] t){
 		int i=t.length-1;
 		boolean houveTrocas=false;
 		do{
 			houveTrocas=false;
 			for(int n=0;n<i;n++){
 				if(t[n].compareTo(t[n+1])>0){
-					Comparable tmp=t[n];
+					T tmp=t[n];
 					t[n]=t[n+1];
 					t[n+1]=tmp;
 					houveTrocas=true;
